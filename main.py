00b18be67b306fe277aea4dc0020a5bfaaebdfef
SPKNout-Kraken <maxvobl2012@gmail.com>
2021-05-24 22:44:50 +0300

    Примеры цикла

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-name = input('Кто создатель python? ')
-while name != 'Гвидо':
-    print('Не верно')
-    name = input('Кто создатель python')
+#Вывод числел от 0 до 100
 
-print('Верно')
+#number = 0
+#while number <= 100:
+#    print(number)
+#    number = number + 1
+
+#Вывод чисел от 0 до n
+
+#number = 0
+#n = int(input('Введите n='))
+#while number <= n:
+#    print(number)
+#    number = number + 1
+
+#Вывод четных чисел от 0 до n
+number = 0
+n = int(input('Введите n='))
+while number <= n:
+    if number % 2 == 0:
+        print(number)
+    number = number + 1
