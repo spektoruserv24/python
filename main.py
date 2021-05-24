9d71b9a4199b4f828476740cf529afa3ad639fbe
SPKNout-Kraken <maxvobl2012@gmail.com>
2021-05-24 13:58:20 +0300

    Программа посчитать период возраста.

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-name = 'Гриня'
-age = 1
-period = 2
-print('Попугайчик')
-print(name)
-print('в')
-print(age)
-print('года')
-print('научился говорить')
-print('своё имя')
-print(name)
-print()
-print('Через')
+birthday_year ='1988'
+print(birthday_year)
+print(type(birthday_year))
+period = 20
 print(period)
-print('года')
-print('Он нучился говорить')
-print('Сколько ему лет')
-print(age + period)
+print(type(period))
+age = int(birthday_year) + period
+print(age)
+sume_str = birthday_year + str(period)
+print(sume_str)
