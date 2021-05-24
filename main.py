34cc54659dbbc3105aa17068c80ee5a8bdb3cbbd
SPKNout-Kraken <maxvobl2012@gmail.com>
2021-05-24 14:19:25 +0300

    Знакомимся с вводом данных.

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-birthday_year ='1988'
-print(birthday_year)
-print(type(birthday_year))
-period = 20
-print(period)
-print(type(period))
-age = int(birthday_year) + period
-print(age)
-sume_str = birthday_year + str(period)
-print(sume_str)
+#result = input()
+#print('пользователь ввел', result)
+name = input('Как тебя зовут?')
+print('Привет', name)
