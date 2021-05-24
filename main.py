172b7ba5adb2c04133dd75a8ad81d795dfed76c2
SPKNout-Kraken <maxvobl2012@gmail.com>
2021-05-24 13:44:39 +0300

    Создали программу про попугайчика

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-print('Hello world')
-
-
-print('Good moring')
+name = 'Гриня'
+age = 1
+period = 2
+print('Попугайчик')
+print(name)
+print('в')
+print(age)
+print('года')
+print('научился говорить')
+print('своё имя')
+print(name)
+print()
+print('Через')
+print(period)
+print('года')
+print('Он нучился говорить')
+print('Сколько ему лет')
+print(age + period)
