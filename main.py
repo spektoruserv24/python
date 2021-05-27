407c60834f0adf45199af744362c5fecbd5b73a9
Spektor_v24-Kraken <max-leders@yandex.ru>
2021-05-27 20:13:52 +0300

    Учимся вырезать текст

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+top5 = 'Первые 5 мест на соревнованиях: 1. Иванов 2. Петров 3. Сидоров 4.Орлов 5. Соколов'
+start = top5.find('1')
+end = top5.find('4')
 
-name = None
+print(start)
 
-while True:
-    name = input('Кто создатель python? ')
-    if name == 'Гвидо':
-        break
-    print('Не верно')
-
-print('Верно')
+top3 = top5[start: end]
+result = 'Поздравляем {} с успехом!' .format(top3.upper())
+print(result)
---
+++ Курс 2/Stroki.py
@@ -0,0 +1,4 @@
+friend = 'Максим'
+
+first_letter = friend[5]
+print(first_letter)
