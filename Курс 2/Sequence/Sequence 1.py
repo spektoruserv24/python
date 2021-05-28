b1e33078df09638899ddbebceeeb74b46869e74c
Spektor_v24-Kraken <max-leders@yandex.ru>
2021-05-28 23:01:55 +0300

    Создание группы Sequence

---
+++ Курс 2/Sequence/Sequence 1.py
@@ -0,0 +1,9 @@
+friend_name = 'Max'
+friends = ['Max', 'Leo', 'Kate']
+roles = ('admin', 'quest', 'user')
+
+if 'Max' in friends:
+    print('У меня есть этот друг')
+
+if 'M' in friend_name:
+    print('Буква M есть в имени друга')
